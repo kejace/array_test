2ef0e928bc549e4f3a549c1a29ad2bf51a6e68fd
kejace <kejace@gmail.com>
2016-02-13 17:28:29 -0500

    Fixed package.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var blockapps = require('blockapps-js')
 
-var contract = "contract C{ \
+var code = "contract C{ \
                   mapping (address => bytes) m; \
-                  function f(address a) { m[a] = \"abcd\";}\
+                  function f() { m[msg.sender] = \"abcd\";}\
                 }";
 
 var privkey = "e011bdbfde66bb78af76aaf907e6bbf2c5715d163524241ae50b5309b40da42d";
@@ -10,11 +10,15 @@ var pub_address = "903b4a914940f08399e41dddcab8e1ea8939cbab";
 
 blockapps.query.serverURI = 'http://strato-dev2.blockapps.net';
 
-blockapps.Solidity(contract).call("newContract", privkey).then(function(x) {contractObj = x}).then(function(v){
+blockapps.Solidity(code).call("newContract", privkey).then(function(contract){
+
+            contract.state.f().callFrom(privkey)
+
+        }).then(function(v){
         
-        console.log("hello contract")
-        contractObj.state.m(0).then(function(v){
-            console.log(v);
-        })
+            console.log("hello contract")
+            v.state.m(0).then(function(v){
+                console.log(v);
+            })
     }
 )
